155e5ac0a6c972ceb6c6aabed6e05b8973a31594
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2024-04-24 08:53:38 +0200

    run web server in main application

--- aid/main.py
+++ aid/main.py
@@ -1,16 +1,21 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+import uvicorn
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from aid.collect.scheduler import construct_scheduler
 from aid.logger import logger
+from aid.query.api import app
 
 
 def main():
-    job_scheduler = construct_scheduler(BlockingScheduler)
+    """Run the complete application."""
+    job_scheduler = construct_scheduler(BackgroundScheduler)
     try:
+        logger.info("Starting scheduler", type=job_scheduler.__class__.__name__)
         job_scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        logger.info("Received exit signal")
-        pass
+        logger.info("Starting web server")
+        uvicorn.run(app)
+    except (KeyboardInterrupt, SystemExit) as exc:
+        logger.info("Graceful exit", signal=exc.__class__.__name__)
 
 
 if __name__ == "__main__":
