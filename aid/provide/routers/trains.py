9a0dd7d8e9a6a2dfb373d8e944000e0f1f61a119
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2024-05-02 19:18:38 +0200

    add keyed train record endpoint

--- aid/provide/routers/trains.py
+++ aid/provide/routers/trains.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 from datetime import datetime, timedelta
+from typing import TypeAlias
 
 from fastapi import APIRouter, Security
+from pydantic import BaseModel
 
 from aid.provide.dependencies import get_api_key
 from aid.provide.ns_trains import TrainRecord, NSTrainProvider
@@ -8,23 +11,54 @@ from aid.provide.ns_trains import TrainRecord, NSTrainProvider
 router = APIRouter(prefix="/trains", tags=["trains"], dependencies=[Security(get_api_key)])
 
 
-@router.get("/current")
-def get_current_train_locations() -> list[TrainRecord]:
-    """
-    Get current train locations (last 10 seconds).
-    """
-    end = datetime.now()
-    start = end - timedelta(seconds=10)
+class TrainLocation(BaseModel):
+    id: int
+    x: float
+    y: float
+    speed: float
+    direction: float
+    accuracy: float
+
+
+KeyedTrainLocations: TypeAlias = dict[datetime, list[TrainLocation]]
+
+
+def _records(start: datetime | None = None, end: datetime | None = None) -> list[TrainRecord]:
+    end = end or datetime.now()
+    start = start or end - timedelta(seconds=10)
     return NSTrainProvider().get_trains(start, end)
 
 
-@router.get("/period")
-def get_train_locations_in_period(start: datetime, end: datetime | None = None) -> list[TrainRecord]:
+@router.get("/records")
+def get_records(start: datetime | None = None, end: datetime | None = None) -> list[TrainRecord]:
     """
-    Get the train locations for a specified period.
+    Get train locations for the requested period as list of records.
 
-    - **start**: start of the requested period (timestamp, required)
+    - **start**: start of the requested period (timestamp, defaults to 10 seconds ago)
     - **end**: end of the requested period (timestamp, defaults to current time)
     """
-    end = end or datetime.now()
-    return NSTrainProvider().get_trains(start, end)
+    return _records(start, end)
+
+
+@router.get("/keyed")
+def get_keyed_records(start: datetime | None = None, end: datetime | None = None) -> KeyedTrainLocations:
+    """
+    Get train locations for the requested period as record sets keyed by timestamp.
+
+    - **start**: start of the requested period (timestamp, defaults to 10 seconds ago)
+    - **end**: end of the requested period (timestamp, defaults to current time)
+    """
+    records = _records(start, end)
+    output: KeyedTrainLocations = defaultdict(list)
+    for record in records:
+        output[record.timestamp].append(
+            TrainLocation(
+                id=record.id,
+                x=record.x,
+                y=record.y,
+                speed=record.speed,
+                direction=record.direction,
+                accuracy=record.accuracy,
+            )
+        )
+    return output
