72353bf6cc977e0da89b414ddacaf73209fcb060
Robin Klaassen <13085028+robinklaassen@users.noreply.github.com>
2024-05-11 10:15:19 +0200

    replace trains view with distinct instead of aggregate

--- ddl/std/trains.sql
+++ ddl/std/trains.sql
@@ -1,9 +1,9 @@
-create view std.trains as
-	        select time_bucket('10 seconds', timestamp, '-5 seconds'::INTERVAL) + '5 seconds' as timestamp, rit_id as id,
-            avg(st_x(st_transform(location, 28992))) as x, avg(st_y(st_transform(location, 28992))) as y,
-            avg(snelheid) as speed, avg(richting) as direction, avg(horizontale_nauwkeurigheid) as accuracy,
-            string_agg(type, ';') as type, string_agg(bron, ';') as source
-        from raw.ns_trains
-        group by timestamp, rit_id
-        order by timestamp asc, rit_id asc
+create or replace view std.trains as
+	select distinct on (timestamp, id)
+	date_bin('10 seconds', timestamp, TIMESTAMP '2001-01-01') as timestamp, rit_id as id,
+	st_x(st_transform(location, 28992)) as x, st_y(st_transform(location, 28992)) as y,
+	snelheid as speed, richting as direction, horizontale_nauwkeurigheid as accuracy,
+	type as type, bron as source
+	from raw.ns_trains
+	order by timestamp asc, rit_id asc
 ;
